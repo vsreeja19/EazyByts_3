0d0f243b1ed793ba325f68c67e9f07096d9081a0
vsreeja19 <varikutisreeja@gmail.com>
2024-07-29 18:08:55 +0530

    Add files via upload

---
+++ news_website/news_website/src/test/java/com/news/news_website/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news.news_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
